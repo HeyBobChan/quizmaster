c2d66c74dd1b8f6fccc9d5f43367d4699778b668
HeyBobChan <newrealityjerusalem@gmail.com>
2024-04-30 17:59:59 +0300

    I am man

--- build_static.py
+++ build_static.py
@@ -1,7 +1,13 @@
 from flask_frozen import Freezer
-from app import app  # Assuming your Flask app is in a file named 'app.py'
+from app import app  # Import your Flask app
 
 freezer = Freezer(app)
 
 if __name__ == '__main__':
+    freezer.register_generator('/')
+    freezer.register_generator('/quiz')
+    freezer.register_generator('/about')
+    freezer.register_generator('/contact')
+    freezer.add_static_rule('/styles', 'styles')
+    freezer.add_static_rule('/images', 'images')
     freezer.freeze()
